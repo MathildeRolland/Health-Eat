7e0139bcbd51a9385d20d076dbe4cf4f752d784f
Mathilde Rolland <rolland.mmathilde@gmail.com>
2021-07-24 17:54:01 +0200

    inscription page done

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types';
 
 import './input.scss';
 
-const Input = ({ type, name, label}) => (
-  <div className="input">
-    <label htmlFor={name}>{label}</label>
-    <input type={type} name={name} />
-  </div>
-);
+const Input = ({ type, name, label, placeholder, value }) => {
+  const className = type === 'submit' ? 'input__input input__input--submit' : 'input__input';
+
+  return (
+    <div className="input">
+      <label className="input__label" htmlFor={name}>{label}</label>
+      <input className={className} type={type} name={name} placeholder={placeholder} value={value} />
+    </div>
+  );
+};
 
 Input.propTypes = {
     type: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    label: PropTypes.string,
 };
 
 export default Input;
--- src/components/Inscription/index.js
+++ src/components/Inscription/index.js
@@ -9,7 +9,12 @@ const Inscription = () => (
   <div className="inscription">
     <Title title="Inscription" />
     <form className="inscription__form">
-        <Input type="text" name="name" label="Nom" />
+        <Input type="text" name="name" label="Nom:" placeholder="Veuillez renseigner votre nom..." />
+        <Input type="text" name="firstname" label="Prénom:" placeholder="Veuillez renseigner votre prénom..." />
+        <Input type="email" name="email" label="Email:" placeholder="Veuillez renseigner votre email..." />
+        <Input type="password" name="password" label="Mot de passe:" placeholder="Veuillez renseigner un mot de passe..." />
+        <Input type="password" name="password-verif" label="Mot de passe:" placeholder="Veuillez de nouveau renseigner votre mot de passe..." />
+        <Input type="submit" value="M'INSCRIRE" />
     </form>
   </div>
 );
